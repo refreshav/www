bb55783a603fb04d1f64fac8c17eeadb6c192c79
michaelcaplan <michael@eggplant.ws>
2016-08-16 07:52:20 -0300

    -FB, fix for off canvas, remove some lipsum

--- themes/f5/static/js/f5.js
+++ themes/f5/static/js/f5.js
@@ -30,19 +30,3 @@ function initMap() {
 }
 
 $(document).foundation();
-
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '641839162645576',
-        xfbml      : true,
-        version    : 'v2.7'
-    });
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
