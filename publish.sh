2a5d713d555564126aeec49a7b3aac19bd02b2ff
michaelcaplan <michael@eggplant.ws>
2016-08-24 19:43:28 -0300

    doh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-hugo -b https://refreshannapilsvalley.org/ -F -d ../RefreshAV.github.io
+hugo -b https://refreshannapolisvalley.org/ -F -d ../RefreshAV.github.io
 git -C ../RefreshAV.github.io add .
 git -C ../RefreshAV.github.io commit -a -m "new build"
 git -C ../RefreshAV.github.io push
